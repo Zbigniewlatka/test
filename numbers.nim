ee6146eb21b78a1fd638b8d221a9d670d256ab7c
Zbigniewlatka <zbychl@gmail.com>
2022-03-02 03:22:36 +0100

    Pierwsze pliki

---
+++ numbers.nim
@@ -0,0 +1,13 @@
+import strutils, sequtils, math
+
+let
+  strNums = readFile("numbers.txt").strip().splitLines()
+  nums = strNums.map(parseFloat)
+
+let
+  sumNums = sum(nums)
+  average = sumNums / float(nums.len)
+
+echo sumNums
+echo average
+
